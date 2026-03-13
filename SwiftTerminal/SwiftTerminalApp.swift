cf6a7e4f40eae6b0f9c1071ac9e0865e47bb70e0
Zabir Raihan <zabirraihan@icloud.com>
2026-03-14 00:05:41 +0600

    Initial Commit

---
+++ SwiftTerminal/SwiftTerminalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTerminalApp.swift
+//  SwiftTerminal
+//
+//  Created by Zabir Raihan on 14/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTerminalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
